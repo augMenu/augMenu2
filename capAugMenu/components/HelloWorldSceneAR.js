b3f329252e7039d222f89c9f630dc52f334357f9
michjadams <Mjadams66@gmail.com>
2018-03-23 12:16:18 -0400

    textures are weird

--- capAugMenu/components/HelloWorldSceneAR.js
+++ capAugMenu/components/HelloWorldSceneAR.js
@@ -24,7 +24,8 @@ export default class HelloWorldSceneAR extends Component {
     // Set initial state here
     this.state = {
       text: "Initializing AR...",
-      showComponent: false
+      showComponent: "0",
+      imageURL: null
     };
     // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
@@ -51,7 +52,11 @@ export default class HelloWorldSceneAR extends Component {
   render() {
     return (
       <ViroARScene ref="arscene" onTrackingInitialized={this._onTrackInit}>
-        <ViroAmbientLight color="#ffffff" intensity={200} />
+        <ViroAmbientLight color="#32CD32" />
+        <ViroSpotLight
+        innerAngle={5} outerAngle={90} direction={[0, -1, -.2]}
+        position={[0, 3, 1]} color="#32CD32" castsShadow={true} />
+
         {this._getNewComponent()}
 
       </ViroARScene>
@@ -60,10 +65,18 @@ export default class HelloWorldSceneAR extends Component {
 
   _getNewComponent() {
 
-    // console.warn("got inside the component")
-    if (this.state.showComponent == true) {
-      return (<ViroBox position={(0, -1, -1)} scale={(.5, .5, .5)} />);
-      return(<Viro3DObject position={(0, -1, -1)} scale={(.5, .5, .5)} source={{uri : 'https://s3.us-east-2.amazonaws.com/augmenu-foodmodels/hamburger/Hamburger.obj'}} />)
+    if (this.state.showComponent == "1") {
+      console.warn("you hit the showComponent thingy, searching this image url", this.state.imageURL)
+      // return (<ViroBox position={(0, -1, -1)} scale={(.5, .5, .5)} />);
+      return(
+          <Viro3DObject type="OBJ" position={[0, 0, -2]} scale={[0.05, 0.05, 0.05]}  
+          //https://s3.us-east-2.amazonaws.com/augmenu-foodmodels/hamburger/Hamburger_BaseColor.jpg
+          //https://s3.us-east-2.amazonaws.com/augmenu-foodmodels/hamburger/Hamburger.mtl
+          resources={[{ uri: 'https://s3.us-east-2.amazonaws.com/augmenu-foodmodels/hamburger/Hamburger.mtl' },
+              {uri:'https://s3.us-east-2.amazonaws.com/augmenu-foodmodels/hamburger/Hamburger_BaseColor.jpg'}]}
+              source={{uri : this.state.imageURL}}
+      shadowCastingBitMask={2} lightRecievingBitMask={3}
+      />)
     } else {
       return (<ViroText onClick={() => this._onClicked()}
         text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />); // return nothing
@@ -76,10 +89,8 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   _onClicked = async () => {
-    console.warn("before Capture")
     let result = await this.props.arSceneNavigator.takeScreenshot('newFile', true);
 
-    console.warn(result.url)
 
     // let image = require(`${result.url}`)
     // console.warn("this is the image", JSON.stringify(image))
@@ -113,40 +124,45 @@ export default class HelloWorldSceneAR extends Component {
     // let foodName = axiosResult.responses[0].textAnnotations[0].description //this might not work 
     // axios.get(`/food:${foodName}`)
     let imageurl = await axios.get('http://172.16.25.156:1337/foods/food/berobj') //need local ip address here when running 
-      .then(res => res.data)
-      .then(food => {
-        
-        // this.setState({imageURL: food.image, showComponent: true})
-        console.warn('this is the state ',typeof food.image)
-      })
-      .catch(err => console.warn(err))
-
-         
+    .then(res => res.data)
+    .then(food => {
+      this.setState({imageURL: food.image})
+      this.setState({showComponent: "1"})
+    })
+    .catch(err => console.warn(err))
+    
+    
+    
     // console.warn(JSON.stringify(axiosResult))
     // responses[0].textAnnotations[0].description
     //  axios.get('/food')
     // .then(res => res.data)
     // .then(food => {
-    // food.image
-    // })
+      // food.image
+      // })
+    }
+    
   }
-
-}
-
-
-var styles = StyleSheet.create({
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-  logoImage:{
-    height: 200,
-    width: 200
-}
-});
-
+  
+  
+  var styles = StyleSheet.create({
+    helloWorldTextStyle: {
+      fontFamily: 'Arial',
+      fontSize: 30,
+      color: '#ffffff',
+      textAlignVertical: 'center',
+      textAlign: 'center',
+    },
+    logoImage:{
+      height: 200,
+      width: 200
+    }
+  });
+  
+  // ViroMaterials.createMaterials({
+  //   grid: {
+  //     diffuseTexture: {uri : this.state.imageURL} || null ,
+  //   },
+  // });
 module.exports = HelloWorldSceneAR;
 //<Image  style={styles.logoImage} source={{uri: '/private/var/mobile/Containers/Data/Application/9165C20B-C1C3-4EC5-93D0-58941817B01A/tmp/viro_media/newFile.png'}}/>
