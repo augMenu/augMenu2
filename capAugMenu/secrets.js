84245a9cea24b8aea3c111ddc6f69f46b0c4c4dc
michjadams <Mjadams66@gmail.com>
2018-03-24 14:40:34 -0400

    testing gitignore again

--- capAugMenu/secrets.js
+++
@@ -1,6 +0,0 @@
-process.env.VIRO_API_KEY = "7E64B933-579F-492D-B242-1E02DD37588B"
-process.env.CLOUD_VISION = "AIzaSyDwp32TG1jOgZcnQYpxRjOSjLG66XbmZSI"
-process.env.AWS_ACCESS_KEY = ""
-process.env.AWS_SEC_KEY = ""
-
-//testing
