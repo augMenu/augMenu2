f2763e3236fc5e3aef7e21a1e6dd05796f76ffa7
DharaNaik <dhararnaik@gmail.com>
2018-03-27 15:57:59 -0400

    Working code after Error handlelling and Cap Menu Item

--- capAugMenu/components/AugMenuScreen.js
+++ capAugMenu/components/AugMenuScreen.js
@@ -83,7 +83,7 @@ export default class HelloWorldSceneAR extends Component {
   }
 
   _getNewComponent() {
-    
+    console.error(`${BASE_URL}/${this.state.menuItem}/materials.mtl`)
     return (
       <Viro3DObject onError={()=>{
         Alert.alert(
